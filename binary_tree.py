5439c19bb9c03e9b819941c88092d86cbb042770
Om Tripathi <omtripa@cisco.com>
2021-04-24 16:30:43 +0530

    .gitignore added

--- binary_tree.py
+++ binary_tree.py
@@ -40,10 +40,10 @@ tree.root.left.right = Node(5)
 
 # Test search
 # Should be True
-print tree.search(4)
+print(tree.search(4))
 # Should be False
-print tree.search(6)
+print(tree.search(6))
 
 # Test print_tree
 # Should be 1-2-4-5-3
-print tree.print_tree()
+print(tree.print_tree())
